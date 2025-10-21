2b44173c271b854906560c58024e0c6d2d4ad131
Asylniet <zasylniet@mail.ru>
2025-10-21 20:03:48 +0500

    feat: 1lecture

--- 1lecture/1.cpp
+++ 1lecture/1.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-  cout << "Hello World!" << endl;
+  // 0 -> '0'
+  // '0' -> 0
+  // char c;
+  // cin >> c;
+  // cout << int(c) - int('0') + 2;
+
+  // "102" -> 102
+  // "102" -> 1 * 100 + 0 * 10 + 2 * 1
+
+  string s;
+  cin >> s;
+  for(int i = 0; i < s.length(); i++) {
+    cout << int(s[i]) - '0' << endl;
+  }
+
+  // XOR
+  // 10 - 1010
+  // 5 - 0101
+  // 15 - 1111
+
+  // 1010
+  // 0101
+  // ----
+  // 1111
+
   return 0;
 }
